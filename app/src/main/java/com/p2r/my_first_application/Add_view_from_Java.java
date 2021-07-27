4b0aa9242d99639a5bdf6314717842c5436a5fa4
RishitKundariya <60743923+RishitKundariya@users.noreply.github.com>
2021-07-27 17:57:05 +0530

    Navigation From One activity to other activity

--- app/src/main/java/com/p2r/my_first_application/Add_view_from_Java.java
+++ app/src/main/java/com/p2r/my_first_application/Add_view_from_Java.java
@@ -1,13 +1,15 @@
 package com.p2r.my_first_application;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
 public class Add_view_from_Java extends AppCompatActivity {
-    Button btClick;
+    Button btClick,btnBack;
     LinearLayout llLayout;
     int count=1;
     @Override
@@ -16,7 +18,15 @@ public class Add_view_from_Java extends AppCompatActivity {
         setContentView(R.layout.activity_add_view_from_java);
         btClick=findViewById(R.id.btnSave);
         llLayout=findViewById(R.id.llmyLayout);
-        Button btn=new Button(this);
+        btnBack=findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),Textbox_Text_Display.class);
+                startActivity(intent);
+                setContentView(R.layout.activity_textbox_text_display);
+            }
+        });
         btClick.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
--- app/src/main/java/com/p2r/my_first_application/Textbox_Text_Display.java
+++ app/src/main/java/com/p2r/my_first_application/Textbox_Text_Display.java
@@ -3,6 +3,7 @@ package com.p2r.my_first_application;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,7 +11,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class Textbox_Text_Display extends AppCompatActivity {
-    TextView tv; Button btnShowText; EditText etInputtextBox;
+    TextView tv; Button btnShowText; EditText etInputtextBox; Button btnNext, btnBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +20,24 @@ public class Textbox_Text_Display extends AppCompatActivity {
         tv=findViewById(R.id.tvShowtext);
         btnShowText=findViewById(R.id.btnShowText);
         etInputtextBox=findViewById(R.id.etInputtextBox);
+         btnNext=findViewById(R.id.btnNext);
+         btnBack=findViewById(R.id.btnBack);
+         btnBack.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+                 startActivity(intent);
+                 setContentView(R.layout.activity_main);
+             }
+         });
+         btnNext.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 Intent intent =new Intent(getApplicationContext(),Add_view_from_Java.class);
+                 startActivity(intent);
+                 setContentView(R.layout.activity_add_view_from_java);
+             }
+         });
 
         btnShowText.setOnClickListener(new View.OnClickListener() {
             @Override
